0b7556e88f5cafc693b40445ed71affe905c7fc5
Sarah Adams <sarah.adams@redspace.com>
2021-03-23 16:25:12 -0300

    [HP-104] Carousel fixes

--- site/assets/js/carousel.js
+++ site/assets/js/carousel.js
@@ -1,17 +1,19 @@
 window.onload = function () {
-  // initialize the carousel
-  var myCarousel = document.querySelector("#homePageCarousel");
-  var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 3000,
-    pause: false,
-  });
-  var carouselPausePlay = document.querySelector(".carousel-pause");
+  // look for the carousel
+  var homePageCarousel = document.querySelector("#homePageCarousel");
 
-  // start the carousel cycling
-  carousel.cycle();
+  if (homePageCarousel) {
+    // if it exists, initialize the carousel
+    var carousel = new bootstrap.Carousel(homePageCarousel, {
+      interval: 3000,
+      pause: false,
+    });
+    var carouselPausePlay = document.querySelector(".carousel-pause");
 
-  // add pause/play functionality to the pause/play button
-  carouselPausePlay &&
+    // start the carousel cycling
+    carousel.cycle();
+
+    // add pause/play functionality to the pause/play button
     carouselPausePlay.addEventListener("click", function (e) {
       if (carousel._isPaused) {
         // go to the next slide immediately, then start cycling
@@ -23,4 +25,5 @@ window.onload = function () {
       e.target.classList.toggle("carousel-pause");
       e.target.classList.toggle("carousel-play");
     });
+  }
 };
