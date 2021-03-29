8a60020e32b8a4918f0636f965e5f8d3f1f8c934
Sarah Adams <sarah.adams@redspace.com>
2021-03-29 15:13:13 -0300

    [HP-543] Add aria attributes etc for carousel a11y

--- site/assets/js/carousel.js
+++ site/assets/js/carousel.js
@@ -6,24 +6,44 @@ window.addEventListener("load", function () {
     // if it exists, initialize the carousel
     var carousel = new bootstrap.Carousel(homePageCarousel, {
       interval: 3000,
-      pause: false,
     });
-    var carouselPausePlay = document.querySelector(".carousel-pause");
+
+    const updateSlide = (e) => {
+      // if (announceItem) {
+      const liveregion = homePageCarousel.querySelector(".liveregion");
+      const slides = homePageCarousel.querySelectorAll(".carousel-item");
+      liveregion.textContent = "Slide " + (e.to + 1) + " of " + slides.length;
+      // }
+    };
+
+    homePageCarousel.addEventListener("slide.bs.carousel", function (e) {
+      updateSlide(e);
+    });
 
     // start the carousel cycling
     carousel.cycle();
 
     // add pause/play functionality to the pause/play button
+    const carouselPausePlay = document.querySelector(".carousel-pause");
     carouselPausePlay.addEventListener("click", function (e) {
+      const button = e.target;
+      const buttonSpan = button.querySelector("span");
+
       if (carousel._isPaused) {
         // go to the next slide immediately, then start cycling
         carousel.next();
         carousel.cycle();
+        buttonSpan.innerHTML = "Stop Animation";
+        button.setAttribute("aria-label", "Pause");
+        button.setAttribute("data-action", "stop");
       } else {
         carousel.pause();
+        buttonSpan.innerHTML = "Start Animation";
+        button.setAttribute("aria-label", "Play");
+        button.setAttribute("data-action", "start");
       }
-      e.target.classList.toggle("carousel-pause");
-      e.target.classList.toggle("carousel-play");
+      button.classList.toggle("carousel-pause");
+      button.classList.toggle("carousel-play");
     });
   }
 });
