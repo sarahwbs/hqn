8e21574d73c816e6d4c8ba87832cc750dec74320
Jonathan Milner <jonathan.milner@redspace.com>
2021-03-31 16:41:09 -0300

    HP-536: Continued implementing the filter component

--- site/assets/js/filter-button.js
+++ site/assets/js/filter-button.js
@@ -1,17 +1,34 @@
 window.addEventListener("load", function () {
-  if (document.querySelector(".filter-button-control")) {
+  if (document.querySelector(".filter-sort-btns__filter-button")) {
     const mainContentDiv = document.querySelector(".main-content");
-    const filterBtn = document.querySelector(
-      ".filter-sort-btns__filter-button"
+    const filterCollapsible = document.querySelector(
+      ".filter-button-control__collapse"
+    );
+    const bsFilterCollapsible = new bootstrap.Collapse(filterCollapsible, {
+      toggle: false
+    });
+    const closeButton = document.querySelector(
+      ".filter-button-control__btn-close"
     );
 
+    function setCloseButtonEventHandler() {
+      // TODO: change css transition to @include transition($transition-collapse);
+      closeButton.addEventListener("click", () => {
+        bsFilterCollapsible.hide();
+      });
+    }
+
+    /**
+     * This function will stack Bootstrap's collapsible elements on top of the main-content div in order
+     * to fix an issue where the Bootstrap's collapsible would stack beneath the main content.
+     */
     function stackElements(action) {
       if (document.body.contains(mainContentDiv)) {
         if (action === "removeStack") {
           mainContentDiv.style.position = "static";
           mainContentDiv.style.zIndex = "unset";
         } else {
-          filterBtn.addEventListener("show.bs.collapse", () => {
+          filterCollapsible.addEventListener("show.bs.collapse", () => {
             mainContentDiv.style.position = "relative";
             mainContentDiv.style.zIndex = 0;
           });
@@ -19,6 +36,7 @@ window.addEventListener("load", function () {
       }
     }
 
-    // stackElements();
+    setCloseButtonEventHandler();
+    stackElements();
   }
 });
