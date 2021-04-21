e4e3989a247517f186f105a7071217dfe8ac3ca0
Sarah Adams <sarah.adams@redspace.com>
2021-04-21 15:13:27 -0300

    [HP-557] Fix carousel state bugs

--- site/assets/js/carousel.js
+++ site/assets/js/carousel.js
@@ -10,24 +10,35 @@ window.addEventListener("load", function () {
     const carouselPausePlay = document.querySelector(".carousel-pause");
     const pausePlayText = carouselPausePlay.querySelector("span");
 
-    function pauseCarousel() {
+    function pauseCarousel(pauseSource) {
+      if (pauseSource && pauseSource === "pause-play") {
+        carouselPausePlay.classList.remove("carousel-pause");
+        carouselPausePlay.classList.add("carousel-play");
+      }
       carousel.pause();
       pausePlayText.innerHTML = "Start Animation";
       carouselPausePlay.setAttribute("aria-label", "Play");
       carouselPausePlay.setAttribute("data-action", "start");
-      carouselPausePlay.classList.remove("carousel-pause");
-      carouselPausePlay.classList.add("carousel-play");
     }
 
-    function unpauseCarousel() {
-      // go to the next slide immediately, then start cycling
-      carousel.next();
+    function unpauseCarousel(pauseSource) {
+      if (
+        pauseSource &&
+        (pauseSource === "pause-play" || pauseSource === "touchend")
+      ) {
+        carouselPausePlay.classList.add("carousel-pause");
+        carouselPausePlay.classList.remove("carousel-play");
+      }
+
+      if (pauseSource && pauseSource === "pause-play") {
+        // go to the next slide immediately before unpausing
+        carousel.next();
+      }
+
       carousel.cycle();
       pausePlayText.innerHTML = "Stop Animation";
       carouselPausePlay.setAttribute("aria-label", "Pause");
       carouselPausePlay.setAttribute("data-action", "stop");
-      carouselPausePlay.classList.add("carousel-pause");
-      carouselPausePlay.classList.remove("carousel-play");
     }
 
     // when the slide updates, change the text in the live region for screen readers
@@ -38,19 +49,38 @@ window.addEventListener("load", function () {
     });
 
     // add pause/play functionality to the pause/play button
-    carouselPausePlay.addEventListener("click", function (e) {
+    carouselPausePlay.addEventListener("click", function () {
       if (carousel._isPaused) {
-        unpauseCarousel();
+        unpauseCarousel("pause-play");
       } else {
-        pauseCarousel();
+        pauseCarousel("pause-play");
       }
     });
 
     // when a user interacts with the carousel using touch events, if the carousel is paused using the pause button, unpause it
     homePageCarousel.addEventListener("touchend", () => {
       if (carousel._isPaused) {
-        unpauseCarousel();
+        unpauseCarousel("touchend");
       }
+    });
+
+    // when a slide receives keyboard focus, if the carousel isn't paused, pause it, when it loses focus, if it was paused due to focus, unpause it
+    const carouselSlideLinks = homePageCarousel.querySelectorAll(
+      ".carousel-link"
+    );
+    carouselSlideLinks.forEach((slide) => {
+      slide.addEventListener("focus", () => {
+        if (!carousel._isPaused) {
+          homePageCarousel.classList.add("focus-paused");
+          pauseCarousel();
+        }
+      });
+      slide.addEventListener("focusout", (e) => {
+        if (homePageCarousel.classList.contains("focus-paused")) {
+          homePageCarousel.classList.remove("focus-paused");
+          unpauseCarousel();
+        }
+      });
     });
 
     // when a user hovers over the prev/next buttons, add a hover class to the carousel item so the hover state doesn't get lost
