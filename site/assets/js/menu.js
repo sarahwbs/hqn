d6a43c60529d6f208a9a1617bd48518ba7636036
Sarah Adams <sarah.adams@redspace.com>
2021-04-27 16:55:27 -0300

    [HP-647] Close the header menu on mouseleave

---
+++ site/assets/js/menu.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function () {
+  const menuHeaderDesktop = document.querySelector(".header__dropdown-menu");
+  const menuMainMobile = document.querySelector("#mainMenuModal");
+  const menuAccountMobile = document.querySelector("#userAccountMenuModal");
+
+  if (menuHeaderDesktop) {
+    const headerAccountMenu = menuHeaderDesktop.closest(".dropdown");
+    const bsHeaderAccountMenu = new bootstrap.Dropdown(
+      headerAccountMenu.querySelector(".btn")
+    );
+
+    headerAccountMenu.addEventListener("mouseleave", () => {
+      bsHeaderAccountMenu.hide();
+    });
+  }
+
+  if (menuHeaderDesktop && menuMainMobile && menuAccountMobile) {
+    const headerAccountMenu = menuHeaderDesktop.closest(".dropdown");
+    const bsHeaderAccountMenu = new bootstrap.Dropdown(
+      headerAccountMenu.querySelector(".btn")
+    );
+    const bsMobileMainMenu = new bootstrap.Modal(menuMainMobile);
+    const bsMobileAccountMenu = new bootstrap.Modal(menuAccountMobile);
+    const mediaQueryList = window.matchMedia("(max-width: 992px)");
+
+    mediaQueryList.addEventListener("change", (e) => {
+      // if less than 992px, hide the desktop dropdown menu
+      if (e.matches) {
+        bsHeaderAccountMenu.hide();
+      }
+      // if more than 992px, hide the mobile menus
+      else {
+        bsMobileMainMenu.hide();
+        bsMobileAccountMenu.hide();
+      }
+    });
+  }
+});
