d7a7e5246aecce9720ecebfbafed14811bb6cbde
Sarah Adams <sarah.adams@redspace.com>
2021-04-30 15:02:29 -0300

    [HP-677] Allow sub opts selection by keyboard

--- site/assets/js/subscription-options.js
+++ site/assets/js/subscription-options.js
@@ -6,18 +6,31 @@ window.addEventListener("load", function () {
     subscriptionOptionButtons = subscriptionOptions.querySelectorAll(
       ".subscription-option__button"
     );
+
+    function selectSubscriptionOption(subscriptionOptionButton) {
+      subscriptionOptionButtons.forEach((button) => {
+        const subscriptionOption = button.closest(".subscription-option");
+        if (subscriptionOptionButton === button) {
+          button.classList.add("checked-button");
+          subscriptionOption.classList.add("selected");
+        } else {
+          button.classList.remove("checked-button");
+          subscriptionOption.classList.remove("selected");
+        }
+      });
+    }
+
     subscriptionOptionButtons.forEach((subscriptionOptionButton) => {
-      subscriptionOptionButton.addEventListener("click", function (e) {
-        subscriptionOptionButtons.forEach((button) => {
-          const subscriptionOption = button.closest(".subscription-option");
-          if (subscriptionOptionButton === button) {
-            button.classList.add("checked-button");
-            subscriptionOption.classList.add("selected");
-          } else {
-            button.classList.remove("checked-button");
-            subscriptionOption.classList.remove("selected");
-          }
-        });
+      subscriptionOptionButton.addEventListener("click", () => {
+        selectSubscriptionOption(subscriptionOptionButton);
+      });
+
+      subscriptionOptionButton.addEventListener("keydown", (e) => {
+        if (e.keyCode == 13 || e.keyCode == 32) {
+          selectSubscriptionOption(subscriptionOptionButton);
+          e.preventDefault();
+          e.stopPropagation();
+        }
       });
     });
   }
