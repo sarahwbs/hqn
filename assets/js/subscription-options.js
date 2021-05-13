efae624242be5e8f16ec0e7e9c8d4878301d6594
Sarah Adams <sarah.adams@redspace.com>
2021-05-13 12:19:29 -0300

    testing

--- assets/js/subscription-options.js
+++ assets/js/subscription-options.js
@@ -29,14 +29,14 @@ window.addEventListener("load", function () {
         .closest(".subscription-option__select")
         .querySelector(".subscription-option__checkmark");
 
-      // subscriptionOptionCheckBox.addEventListener("change", () => {
-      //   selectSubscriptionOption(subscriptionOptionButton);
-      // });
-
-      subscriptionOptionButton.addEventListener("touchstart", () => {
+      subscriptionOptionCheckBox.addEventListener("change", () => {
         alert("hi");
         selectSubscriptionOption(subscriptionOptionButton);
       });
+
+      // subscriptionOptionButton.addEventListener("touchstart", () => {
+      //   selectSubscriptionOption(subscriptionOptionButton);
+      // });
 
       subscriptionOptionButton.addEventListener("keydown", (e) => {
         if (e.keyCode == 13 || e.keyCode == 32) {
