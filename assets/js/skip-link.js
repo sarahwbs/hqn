687dc8e53da79d504be3a46530312bb4e490a760
Sarah Adams <sarah.adams@redspace.com>
2021-05-18 15:16:19 -0300

    r

---
+++ assets/js/skip-link.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+  // when a slide receives keyboard focus, if the carousel isn't paused, pause it, when it loses focus, if it was paused due to focus, unpause it
+  const skipLinks = document.querySelectorAll(".skip-nav");
+  skipLinks.forEach((skipLink) => {
+    skipLink.addEventListener("focus", () => {
+      skipLink.classList.add("focus");
+    });
+    skipLink.addEventListener("focusout", () => {
+      if (homePageCarousel.classList.contains("focus-paused")) {
+        skipLink.classList.remove("focus");
+      }
+    });
+  });
+});
