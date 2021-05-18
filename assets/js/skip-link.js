e5efd84dded78e7a2fea56c53f7d42dedf530b84
Sarah Adams <sarah.adams@redspace.com>
2021-05-18 16:03:53 -0300

    T

--- assets/js/skip-link.js
+++ assets/js/skip-link.js
@@ -2,6 +2,9 @@ window.addEventListener("load", function () {
   // when a slide receives keyboard focus, if the carousel isn't paused, pause it, when it loses focus, if it was paused due to focus, unpause it
   const skipLinks = document.querySelectorAll(".skip-nav");
   skipLinks.forEach((skipLink) => {
+    // skipLink.addEventListener("click", () => {
+    //   document.querySelector("#main").focus();
+    // });
     skipLink.addEventListener("focus", () => {
       skipLink.classList.add("focus");
     });
